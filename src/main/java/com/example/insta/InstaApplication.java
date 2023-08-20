8e6d088f3c34739a7bc62fcc231a05af637cdc17
Rene Wenz <wenz@spengergasse.at>
2023-08-20 10:05:54 +0200

    add logging in application main

--- src/main/java/com/example/insta/InstaApplication.java
+++ src/main/java/com/example/insta/InstaApplication.java
@@ -3,11 +3,20 @@ package com.example.insta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+              // test test test
 @SpringBootApplication
-public class InstaApplication {
+public class InstaApplication
+              {
 
   public static void main(String[] args) {
 
-    SpringApplication.run(InstaApplication.class, args);
+            System.out.println(
+
+
+                "Hello World");
+
+
+
+            SpringApplication.run(InstaApplication.class, args);
   }
 }

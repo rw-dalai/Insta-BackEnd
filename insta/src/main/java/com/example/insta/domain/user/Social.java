ea0459907243297436a10ee0433a01f135534251
Rene Wenz <wenz@spengergasse.at>
2023-10-02 17:57:49 +0200

    update insta docs

--- insta/src/main/java/com/example/insta/domain/user/Social.java
+++ insta/src/main/java/com/example/insta/domain/user/Social.java
@@ -7,12 +7,11 @@ import java.util.Map;
 /**
  * Relationships of a user.
  *
- * <p>Here we can place the relationships of a user. Following, followers, blocked users, etc.
+ * <p>Here we can place the relationships of a user. Following, followers, blocked users, etc. <br>
  *
- * <p>EXAMPLE: relations: { "123": { "type": "INCOMING", "createdAt": "2021-01-01T00:00:00Z" },
- * "456": { "type": "OUTGOING", "createdAt": "2021-01-01T00:00:00Z" }, } In this example, the user
- * has two relationships. The first one is an incoming relationship with the user whose id is "123".
- * The second one is an outgoing relationship with the user whose id is "456".
+ * <p>EXAMPLE: relations: { "123": { "type": "INCOMING", "createdAt": "2021-01-01T00:00:00Z" } }
+ * <br>
+ * The user has one relation one is an incoming friend request from user with id 123.
  */
 
 // This class in inlined in User.

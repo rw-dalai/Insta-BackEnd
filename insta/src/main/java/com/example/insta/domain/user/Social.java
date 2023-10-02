192c264e802efb179f04b97920794be62cd173cd
Rene Wenz <wenz@spengergasse.at>
2023-10-02 18:56:52 +0200

    update insta docs

--- insta/src/main/java/com/example/insta/domain/user/Social.java
+++ insta/src/main/java/com/example/insta/domain/user/Social.java
@@ -9,14 +9,13 @@ import java.util.Map;
  *
  * <p>Here we can place the relationships of a user. <br>
  * Following, followers, blocked users, etc. <br>
- *
- * <p>EXAMPLE: <br>
- * relations: { "1": { "type": "INCOMING", "createdAt": "2021-01-01T00:00:00Z" } } <br>
- * The user has one relation one is an incoming friend request from user with id 1.
  */
 
 // This class in inlined in User.
 public class Social {
+  // A map of user ids to relations (key=fromUserId, value=relation)
+  // Example: { "123": { "type": "INCOMING", "createdAt": "2023-01-01T00:00:00Z" } }
+  // In this example our user has a friend request from user with id 123.
   private final Map<String, Relation> relations = new HashMap<>();
 
   public record Relation(RelationType type, Instant createdAt) {}

eef9683ea02d6a24fa256c63ce1c441707b9b8c9
Rene Wenz <wenz@spengergasse.at>
2023-11-01 21:02:11 +0100

    add user repository methods and tests

---
+++ insta/src/main/java/com/example/insta/config/MongoConfig.java
@@ -0,0 +1,8 @@
+package com.example.insta.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {}
--- insta/src/main/java/com/example/insta/persistence/UserRepository.java
+++ insta/src/main/java/com/example/insta/persistence/UserRepository.java
@@ -1,8 +1,15 @@
 package com.example.insta.persistence;
 
 import com.example.insta.domain.user.User;
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+// https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.query-methods.query-lookup-strategies
+
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {}
+public interface UserRepository extends MongoRepository<User, String> {
+
+  Optional<User> findByEmail(String email);
+  //    User findByEmail(String email);
+}
